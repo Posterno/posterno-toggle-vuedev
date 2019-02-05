13ebf352719c78d50e0b7a096f4467463c501116
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-02-05 16:18:32 +0000

    reset plugin names and description

--- posterno-toggle-vuedev.php
+++ posterno-toggle-vuedev.php
@@ -1,15 +1,13 @@
 <?php
 /**
  * Plugin Name:     Posterno Toggle Vuedev
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://posterno.com
+ * Description:     Toggle Vuejs development mode for Posterno directly from the WordPress admin bar.
+ * Author:          Posterno, Alessandro Tesoro
+ * Author URI:      https://posterno.com
  * Text Domain:     posterno-toggle-vuedev
  * Domain Path:     /languages
  * Version:         0.1.0
  *
  * @package         Posterno_Toggle_Vuedev
  */
-
-// Your code starts here.
