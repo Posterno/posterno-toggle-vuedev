97ea3d3c164c3f4fcafb0e6092ccd7f616223d6e
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-02-05 16:16:14 +0000

    hello world

---
+++ posterno-toggle-vuedev.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Posterno Toggle Vuedev
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     posterno-toggle-vuedev
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Posterno_Toggle_Vuedev
+ */
+
+// Your code starts here.
